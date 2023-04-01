5d89ccd1cf3174efa0800e0e68c172ef65f7ef83
shaw <shawwian@gmail.com>
2023-04-01 02:34:17 -0400

    Opcode Patterns in 8086 Arithmetic

--- decoder/decoder.c
+++ decoder/decoder.c
@@ -4,7 +4,6 @@
 #define SINGLE_INST_BUF_SIZE    64
 #define OPERAND_BUF_SIZE        32
 
-
 typedef struct {
 	uint16_t opcode : 6;
 	uint16_t dir    : 1;
@@ -22,11 +21,27 @@ char *regs[2][8] = {
 	{ "al", "cl", "dl", "bl", "ah", "ch", "dh", "bh" },
 	{ "ax", "cx", "dx", "bx", "sp", "bp", "si", "di" },
 };
+
 char *effective_address_table[8] = {
 	"bx + si", "bx + di", "bp + si", "bp + di",
 	"si", "di", "bp", "bx",
 };
-char *stream;
+
+char *arithmetic_ops[] = {
+	[0x0] = "add",
+	[0x5] = "sub",
+	[0x7] = "cmp",
+};
+
+char *conditional_jump_names[] = {
+	[0x70] = "jo",     [0x71] = "jno",   [0x72] = "jb",   [0x73] = "jnb",
+	[0x74] = "je",     [0x75] = "jne",   [0x76] = "jbe",  [0x77] = "ja",
+	[0x78] = "js",     [0x79] = "jns",   [0x7A] = "jp",   [0x7B] = "jnp",
+	[0x7C] = "jl",     [0x7D] = "jnl",   [0x7E] = "jle",  [0x7F] = "jg",
+	[0xE0] = "loopnz", [0xE1] = "loopz", [0xE2] = "loop", [0xE3] = "jcxz",
+};
+
+uint8_t *stream;
 
 char *reg_name(uint8_t wide, uint8_t reg) {
 	return regs[wide][reg];
@@ -50,12 +65,10 @@ void buf_printf(char **buf, char *fmt, ...) {
 int decode_displacement(Instruction *inst) {
 	int disp_bytes = 0;
 	if (inst->mode == 0x1) {
-		inst->disp = stream[2] & 0xFF;
+		inst->disp = stream[2];
 		disp_bytes = 1;
 	} else if (inst->mode == 0x2 || (inst->mode == 0x0 && inst->r_m == 0x6)) {
-		uint16_t lo = stream[2] & 0xFF;
-		uint16_t hi = stream[3] & 0xFF;
-		inst->disp = (hi << 8) | lo;
+		inst->disp = ((uint16_t)stream[3] << 8) | stream[2];
 		disp_bytes = 2;
 	} else {
 		assert(inst->mode == 0x0 || inst->mode == 0x3);
@@ -63,7 +76,47 @@ int decode_displacement(Instruction *inst) {
 	return disp_bytes;
 }
 
-Instruction decode_mov_instruction(void) {
+bool is_arithmetic(Instruction *inst) {
+	// HACK: this only works because were currently only considering mov and arithmtic ops
+	// and unconditional jumps, but that just happens not to interfere
+	return inst->opcode < 0x22;
+}
+
+bool imm_to_rm(Instruction *inst) {
+	return inst->opcode == 0x31 || inst->opcode == 0x20;
+}
+
+bool rm_to_from_reg(Instruction *inst) {
+	return inst->opcode == 0x22 || (inst->opcode & 0x31) == 0x0;
+}
+
+bool imm_to_acc(Instruction *inst) {
+	return (inst->opcode & 0x31) == 0x01;
+}
+
+// lo and hi are offsets of the low and high immediate byte, from the first byte of the instruction
+int decode_immediate(Instruction *inst, int lo, int hi) {
+	int data_bytes = 1;
+	inst->data = stream[lo];
+
+	bool arithemtic_imm_rm = is_arithmetic(inst) && imm_to_rm(inst);
+
+	if ((arithemtic_imm_rm && inst->wide && !inst->dir) ||
+		(!arithemtic_imm_rm && inst->wide)) 
+	{
+		inst->data |= (uint16_t)stream[hi] << 8;
+		data_bytes = 2;
+	}
+
+	return data_bytes;
+}
+
+bool is_conditional_jump(uint8_t opcode) {
+	return (opcode > 0x6F && opcode < 0x80) ||
+	       (opcode > 0xDF && opcode < 0xE4);
+}
+
+Instruction decode_instruction(void) {
 	Instruction inst = {
 		.opcode = (stream[0] & 0xFC) >> 2,
 		.dir    = (stream[0] >> 1) & 1,
@@ -77,43 +130,37 @@ Instruction decode_mov_instruction(void) {
 	int data_bytes = 0;
 	int addr_bytes = 0;
 
-	// immediate to register
-	if ((stream[0] & 0xB0) == 0xB0) {
+	// mov immediate to register
+	if ((stream[0] & 0xF0) == 0xB0) {
 		inst.wide = (stream[0] >> 3) & 1;
 		inst.reg = stream[0] & 0x7; 
-		inst.data = stream[1] & 0xFF;
-		if (inst.wide) { 
-			uint16_t hi = stream[2] & 0xFF;
-			inst.data |= hi << 8;
-			data_bytes = 1;
-			// NOTE(shaw): data_bytes is only 1 here, because in immediate to register 
-			// the lo data byte clobbers the mod reg r/m byte used by other instructions
-		}
-
-	// register/memory to/from register
-	} else if (inst.opcode == 0x22) {
+		// NOTE(shaw): the minus 1 here is because in immediate to register 
+		// the lo data byte clobbers the mod reg r/m byte used by other instructions
+		data_bytes = decode_immediate(&inst, 1, 2) - 1;
+		
+	} else if (rm_to_from_reg(&inst)) {
 		disp_bytes = decode_displacement(&inst);
 
-	// immediate to register/memory
-	} else if (inst.opcode == 0x31) {
+	} else if (imm_to_rm(&inst)) {
 		disp_bytes = decode_displacement(&inst);
-		inst.data = stream[4] & 0xFF;
-		data_bytes = 1;
-		if (inst.wide) { 
-			uint16_t hi = stream[5] & 0xFF;
-			inst.data |= hi << 8;
-			data_bytes = 2;
-		}
+		data_bytes = decode_immediate(&inst, 2+disp_bytes, 3+disp_bytes);
+
+	} else if (imm_to_acc(&inst)) {
+		// NOTE(shaw): the minus 1 here is because in immediate to accumulator 
+		// the lo data byte clobbers the mod reg r/m byte used by other instructions
+		data_bytes = decode_immediate(&inst, 1, 2) - 1;
 	
-	// memory to accumulator & vice versa
+	// mov memory to accumulator & vice versa
 	} else if (inst.opcode == 0x28) {
-		inst.addr = (stream[2] << 8) | stream[1];
+		inst.addr = ((uint16_t)stream[2] << 8) | stream[1];
 		addr_bytes = 2;
 
-	// register/memory to segment register & vice versa
+	// mov register/memory to segment register & vice versa
 	} else if (inst.opcode == 0x23) {
 		disp_bytes = decode_displacement(&inst);
 
+	} else if (is_conditional_jump(stream[0])) {
+		inst.disp = stream[1];
 	} else {
 		assert(0);
 	}
@@ -131,6 +178,10 @@ char *effective_address(Arena *arena, Instruction *inst) {
 	char *addr = arena_alloc_zeroed(arena, OPERAND_BUF_SIZE);
 	char *buf_ptr = addr;
 
+	if (imm_to_rm(inst) && !is_reg_mode(inst->mode)) {
+		buf_printf(&buf_ptr, "%s ", inst->wide ? "word" : "byte");
+	}
+
 	if (is_direct_address(inst)) {
 		buf_printf(&buf_ptr, "[%d]", inst->disp);
 	} else {
@@ -147,18 +198,37 @@ char *effective_address(Arena *arena, Instruction *inst) {
 	return addr;
 }
 
-char *disassemble_mov_instruction(Arena *arena, Instruction *inst) {
+char *disassemble_conditional_jump(Arena *arena, Instruction *inst) {
+	// FIXME: have to recompute the full opcode since we are only storing 6 bits of opcode directly
+	// should probably store 8 bit opcode all the time, but there are a lot of places where the 6 
+	// bit version is used with bitmasks and will need to go change all of those. im lazy rn
+	uint8_t full_opcode = ((uint8_t)inst->opcode << 2) | inst->dir << 1 | inst->wide;
+	int8_t displacement = (int8_t)inst->disp;
+	
+	// longest conditional jump is 6 chars + space + largest 8bit signed is 4 chars
+	int len = 16; 
+	char *asm_inst = arena_alloc_zeroed(arena, len);
+	snprintf(asm_inst, len, "%s %d", conditional_jump_names[full_opcode], displacement);
+	return asm_inst;
+}
+
+char *disassemble_instruction(Arena *arena, Instruction *inst) {
+	uint8_t full_opcode = ((uint8_t)inst->opcode << 2) | inst->dir << 1 | inst->wide;
+	if (is_conditional_jump(full_opcode)) {
+		return disassemble_conditional_jump(arena, inst);
+	}
+
 	char *src, *dst;
 
-	// immediate to register
-	if ((inst->opcode & 0x2C) == 0x2C) {
+	// mov immediate to register
+	if ((inst->opcode & 0x3C) == 0x2C) {
 		// NOTE: max immediate is 65536, so 8 is enough chars
 		src = arena_alloc_zeroed(arena, 8);
 		snprintf(src, 8, "%d", inst->data);
 		dst = reg_name(inst->wide, inst->reg);
 
 	// register/memory to/from register
-	} else if (inst->opcode == 0x22) {
+	} else if (rm_to_from_reg(inst)) {
 		if (inst->dir == 0) {
 			// src is reg, dst is r/m
 			src = reg_name(inst->wide, inst->reg);
@@ -174,7 +244,7 @@ char *disassemble_mov_instruction(Arena *arena, Instruction *inst) {
 		}
 
 	// immediate to register/memory
-	} else if (inst->opcode == 0x31) {
+	} else if (imm_to_rm(inst)) {
 		// NOTE: max immediate is 65536, so 8 is enough chars
 		src = arena_alloc_zeroed(arena, 8);
 		snprintf(src, 8, "%d", inst->data);
@@ -182,19 +252,32 @@ char *disassemble_mov_instruction(Arena *arena, Instruction *inst) {
 				? reg_name(inst->wide, inst->r_m)
 				: effective_address(arena, inst);
 
+	// immediate to accumulator
+	} else if (imm_to_acc(inst)) {
+		dst = reg_name(inst->wide, 0); // accumulator
+		src = arena_alloc_zeroed(arena, 8);
+		snprintf(src, 8, "%d", inst->data);
+
 	// memory to accumulator & vice versa
 	} else if (inst->opcode == 0x28) {
 		assert(0);
 	// register/memory to segment register & vice versa
 	} else if (inst->opcode == 0x23) {
 		assert(0);
-	} else {
-		assert(0);
-	}
+	} 
 	
 	int len = 4 + strlen(dst) + 2 + strlen(src) + 1;
 	char *asm_inst = arena_alloc(arena, len);
-	snprintf(asm_inst, len, "mov %s, %s", dst, src);
+
+	if (is_arithmetic(inst)) {
+		int op = imm_to_rm(inst) 
+			? inst->reg 
+			: (inst->opcode >> 1) & 0x7;
+		snprintf(asm_inst, len, "%s %s, %s", arithmetic_ops[op], dst, src);
+	}
+	else
+		snprintf(asm_inst, len, "mov %s, %s", dst, src);
+
 	asm_inst[len] = 0;
 	return asm_inst;
 }
@@ -225,11 +308,11 @@ int main(int argc, char **argv) {
 
 	buf_printf(&buf_ptr, "bits 16\n");
 
-	stream = file_data;
+	stream = (uint8_t*)file_data;
 
 	while (stream < file_data + file_size) {
-		Instruction inst = decode_mov_instruction();
-		char *asm_inst = disassemble_mov_instruction(&string_arena, &inst);
+		Instruction inst = decode_instruction();
+		char *asm_inst = disassemble_instruction(&string_arena, &inst);
 		buf_printf(&buf_ptr, "%s\n", asm_inst);
 	}
 	fclose(fp);
--- decoder/test.asm
+++ decoder/test.asm
@@ -1,15 +1,97 @@
 bits 16
-mov si, bx
-mov dh, al
-mov cx, 12
-mov cx, 65524
-mov dx, 3948
-mov dx, 61588
-mov al, [bx + si]
-mov bx, [bp + di]
-mov dx, [bp + 0]
-mov ah, [bx + si + 4]
-mov al, [bx + si + 4999]
-mov [bx + di], cx
-mov [bp + si], cl
-mov [bp + 0], ch
+add bx, [bx + si]
+add bx, [bp + 0]
+add si, 2
+add bp, 2
+add cx, 8
+add bx, [bp + 0]
+add cx, [bx + 2]
+add bh, [bp + si + 4]
+add di, [bp + di + 6]
+add [bx + si], bx
+add [bp + 0], bx
+add [bp + 0], bx
+add [bx + 2], cx
+add [bp + si + 4], bh
+add [bp + di + 6], di
+add byte [bx], 34
+add word [bp + si + 1000], 29
+add ax, [bp + 0]
+add al, [bx + si]
+add ax, bx
+add al, ah
+add ax, 1000
+add al, 226
+add al, 9
+sub bx, [bx + si]
+sub bx, [bp + 0]
+sub si, 2
+sub bp, 2
+sub cx, 8
+sub bx, [bp + 0]
+sub cx, [bx + 2]
+sub bh, [bp + si + 4]
+sub di, [bp + di + 6]
+sub [bx + si], bx
+sub [bp + 0], bx
+sub [bp + 0], bx
+sub [bx + 2], cx
+sub [bp + si + 4], bh
+sub [bp + di + 6], di
+sub byte [bx], 34
+sub word [bx + di], 29
+sub ax, [bp + 0]
+sub al, [bx + si]
+sub ax, bx
+sub al, ah
+sub ax, 1000
+sub al, 226
+sub al, 9
+cmp bx, [bx + si]
+cmp bx, [bp + 0]
+cmp si, 2
+cmp bp, 2
+cmp cx, 8
+cmp bx, [bp + 0]
+cmp cx, [bx + 2]
+cmp bh, [bp + si + 4]
+cmp di, [bp + di + 6]
+cmp [bx + si], bx
+cmp [bp + 0], bx
+cmp [bp + 0], bx
+cmp [bx + 2], cx
+cmp [bp + si + 4], bh
+cmp [bp + di + 6], di
+cmp byte [bx], 34
+cmp word [4834], 29
+cmp ax, [bp + 0]
+cmp al, [bx + si]
+cmp ax, bx
+cmp al, ah
+cmp ax, 1000
+cmp al, 226
+cmp al, 9
+jne 2
+jne -4
+jne -6
+jne -4
+je -2
+jl -4
+jle -6
+jb -8
+jbe -10
+jp -12
+jo -14
+js -16
+jne -18
+jnl -20
+jg -22
+jnb -24
+ja -26
+jnp -28
+jno -30
+jns -32
+loop -34
+loopz -36
+loopnz -38
+jcxz -40
