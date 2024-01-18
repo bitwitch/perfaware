b49e7486efe5a1cc6ca123a13772b699846d7b1c
Shaw <shawwian@gmail.com>
2024-01-17 21:29:24 -0500

    squash

--- repetition/build.bat
+++ repetition/build.bat
@@ -11,7 +11,7 @@ if "%1" == "fault_counter" (
 	cl /W3 /WX /Zi /fsanitize=address /nologo "%source_dir%test_fread.c" 
 	cl /O2 /W3 /WX /Zi /nologo /Fetest_fread_release "%source_dir%test_fread.c"
 ) else (
-	cl /W3 /WX /Zi /fsanitize=address /nologo "%source_dir%test_front_end.c" /link /LIBPATH:%source_dir%
-	cl /O2 /W3 /WX /Zi /nologo /Fetest_front_end_release "%source_dir%test_front_end.c" /link /LIBPATH:%source_dir%
+	cl /W3 /WX /Zi /fsanitize=address /nologo "%source_dir%test_asm_loops.c" /link /LIBPATH:%source_dir%
+	cl /O2 /W3 /WX /Zi /nologo /Fetest_asm_loops_release "%source_dir%test_asm_loops.c" /link /LIBPATH:%source_dir%
 )
 popd
