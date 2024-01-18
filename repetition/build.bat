89e1a1715691c4f1f1c8f6123db9c28c0b678abe
Shaw <shawwian@gmail.com>
2024-01-17 21:26:47 -0500

    testing loops in asm

--- repetition/build.bat
+++ repetition/build.bat
@@ -1,10 +1,17 @@
 @echo off
+setlocal
 if not exist build\ mkdir build
 pushd build
+
+set "source_dir=%~dp0"
+
 if "%1" == "fault_counter" (
-	cl /W3 /WX /Zi /nologo "%~dp0fault_counter.c"
+	cl /W3 /WX /Zi /nologo "%source_dir%fault_counter.c"
+) else if "%1" == "test_fread" (
+	cl /W3 /WX /Zi /fsanitize=address /nologo "%source_dir%test_fread.c" 
+	cl /O2 /W3 /WX /Zi /nologo /Fetest_fread_release "%source_dir%test_fread.c"
 ) else (
-	cl /W3 /WX /Zi /fsanitize=address /nologo "%~dp0test_fread.c"
-	cl /O2 /W3 /WX /Zi /nologo /Fetest_fread_release "%~dp0test_fread.c"
+	cl /W3 /WX /Zi /fsanitize=address /nologo "%source_dir%test_asm_loops.c" /link /LIBPATH:%source_dir%
+	cl /O2 /W3 /WX /Zi /nologo /Fetest_asm_loops_release "%source_dir%test_asm_loops.c" /link /LIBPATH:%source_dir%
 )
 popd
